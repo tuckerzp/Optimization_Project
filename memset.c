f619f42580946066c59d1efa34625fa26c9ee5de
Ryan Showalter <ryanshowalter76@gmail.com>
2019-11-05 19:18:31 -0500

    Add a simple unit test

---
+++ memset.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "memset.h"
+
+/* Basic implementation of memset */
+void *basic_memset(void *s, int c, size_t n)
+{
+    size_t cnt = 0;
+    unsigned char *schar = s;
+    while (cnt < n) {
+        *schar++ = (unsigned char) c;
+        cnt++;
+    }
+    return s;
+}
