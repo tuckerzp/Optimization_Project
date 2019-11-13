69e577d2d0d3a1c3786e426a00833232e596ed1e
z-intercepts <zachptucker@gmail.com>
2019-11-13 13:49:03 -0500

    Make size of a static

--- h_main.c
+++ h_main.c
@@ -14,20 +14,17 @@ int main(int argc, char **argv) {
     long degree = strtol(argv[1], NULL, 10);
     long i;
     double x = 2.15;
-    double a[degree];
+    double a[200000]; // 200,000 elements
     double p;
 
-    for (i = 0; i + 5 < degree; i++) {
+    for (i = 0; i + 5 < 200000; i++) {
         a[i] = 1.1;
         a[i + 1] = 2.2;
         a[i + 2] = 3.3;
         a[i + 3] = 4.4;
         a[i + 4] = 5.5;
     }
-    for (; i < degree; i++) {
-        a[i] = 6.6;
-    }
-
+    
     // p = poly(a, x, degree);
     // p = polyh(a, x, degree;
 }
